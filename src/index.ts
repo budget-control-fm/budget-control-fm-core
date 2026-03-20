df2ec41381c32f10398521f6b0c27dba7f027ac2
fabiomoggi <fabio.moggi@gmail.com>
2026-03-20 02:35:28 -0300

    feature m4 (#103)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export const hello = () => "Hi budget-control-fm-core";
 export const helloWorld = () => "Hello world!";
 export const helloUniverse = () => "Hello universe!";
+export const helloGalaxy = () => "Hello galaxy!";
