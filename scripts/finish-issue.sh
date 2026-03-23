59ec832a62b5edac4e4441ac6b4dda1d414e5f59
Fabio Moggi <fabio.moggi@gmail.com>
2026-03-23 12:08:45 -0300

    updated

--- scripts/finish-issue.sh
+++ scripts/finish-issue.sh
@@ -11,16 +11,22 @@ fi
 
 echo "→ Running tests..."
 npm run test:coverage
+echo "______________________________"
 
-echo "\n→ Running tests..."
+echo "→ Running tests..."
 git pull origin $BRANCH
+echo "______________________________"
+
 
 echo "→ Committing..."
 git add .
 git commit -m "$COMMIT"
+echo "______________________________"
+
 
 echo "→ Pushing $BRANCH..."
 git push -u origin "$BRANCH"
+echo "______________________________"
 
 echo "✓ Done. Open your PR at:"
 echo "  https://github.com/budget-control-fm/budget-control-fm-core/compare/$BRANCH"
