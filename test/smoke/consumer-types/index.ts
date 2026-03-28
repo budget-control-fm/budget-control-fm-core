e212f8ca9d2bd88390147a749666af29988c8994
fabiomoggi <fabio.moggi@gmail.com>
2026-03-28 00:02:33 -0300

    test ok (#211)

--- test/smoke/consumer-types/index.ts
+++
@@ -1,7 +0,0 @@
-import { RegisterUserUseCase } from "budget-control-fm-core/user";
-
-const value: unknown = RegisterUserUseCase;
-
-if (!value) {
-  throw new Error("TypeScript smoke test failed");
-}
